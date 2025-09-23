95cd6e70b310d6c1595bafc6a12f3c9f9ec3cb57
nane-andreasyan <nane.andreasyan@gmail.com>
2025-09-23 17:23:48 +0000

    hw4

---
+++ HW4/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "math_utils.h"
+int main() {
+    int x = 0;
+    int y = 10;
+    printf("a = %d, b = %d\n", x, y);
+    printf("Sum: %d\n", add(x, y));
+    printf("Multiplication: %d\n", multiply(x, y));
+}
+
+
+
+
+ 
---
+++ HW4/math_utils.h
@@ -0,0 +1,8 @@
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+
+int add(int x, int y);
+
+int multiply(int x, int y);
+
+#endif
